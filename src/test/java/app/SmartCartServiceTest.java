0c293473c3444bc21b815e3f933de94d372f7944
K.Kosmachevskiy <k.kosmachevskiy@gmail.com>
2017-07-10 20:29:43 +0300

    Initial commit

---
+++ src/test/java/app/SmartCartServiceTest.java
@@ -0,0 +1,53 @@
+package app;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore
+public class SmartCartServiceTest {
+
+    private SmartCartService service;
+
+    @Test
+    public void name() throws Exception {
+        String wrongKey = "FFFFFFFFFFFA";
+        try {
+            service = new SmartCartService(60, wrongKey);
+            service.erase();
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertEquals("Authentication error", e.getMessage());
+        }
+    }
+
+    @Test
+    public void test() throws Exception {
+        service = new SmartCartService(60, "FFFFFFFFFFFF");
+
+        service.erase();
+        byte[] read = service.read();
+        Assert.assertArrayEquals(new byte[0], read);
+
+        String data = "TEST_DATA_123";
+        service.write(data.getBytes());
+        Assert.assertEquals("TEST_DATA_123", new String(service.read()));
+
+        service.write("123456789012345".getBytes());
+        Assert.assertEquals("123456789012345", new String(service.read()));
+
+        service.write("1".getBytes());
+        Assert.assertEquals("1", new String(service.read()));
+
+        service.write("String");
+        String s = service.readString();
+        Assert.assertEquals("String", s);
+
+        try {
+            service.write("Too long String!!!");
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertEquals("Data too large. Max size is 15 chars. Size of data buffer = 18", e.getMessage());
+        }
+    }
+}
